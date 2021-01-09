37aee04aa7eef8bd38973a112e3d8b65fee30d7a
Prince Agrawal <princeagrawalnew123@gmail.com>
2021-01-09 19:43:18 +0530

    cleanup, prettier and eslint setup to run on each commit

---
+++ .prettierrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  // Change your rules accordingly to your coding style preferencies.
+  // https://prettier.io/docs/en/options.html
+  semi: true,
+  trailingComma: 'es5',
+  singleQuote: true,
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  parser: 'babel',
+  arrowParens: 'always',
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  htmlWhitespaceSensitivity: 'css',
+
+  overrides: [
+    {
+      files: '*.{css,scss}',
+      options: {
+        parser: 'css',
+      },
+    },
+  ],
+};
